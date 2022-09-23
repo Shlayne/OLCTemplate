6d33dc970c3004c19926dbc5269d8420b0b04ebe
Bruce Cosgrove <1901.brc@gmail.com>
2022-09-22 18:24:54 -0700

    This made it not work apparently.

--- Scripts/GenerateProjects.bat
+++ Scripts/GenerateProjects.bat
@@ -2,6 +2,5 @@
 pushd ..
 call Dependencies\premake\premake5.exe vs2022
 popd
-REM https://stackoverflow.com/questions/3551888/pausing-a-batch-file-when-double-clicked-but-not-when-run-from-a-console-window
 for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
 if defined DOUBLECLICKED pause
